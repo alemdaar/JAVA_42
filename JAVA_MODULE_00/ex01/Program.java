37a133e5893d25f6b5b7a12462909983fac90333
Oussama Elhassouni <oelhasso@c1r5p7.1337.ma>
2026-09-03 18:46:36 +0100

    finished ex01

---
+++ JAVA_MODULE_00/ex01/Program.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main (String[] args)
+    {
+        int iterations = 2;
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number <= 1) {
+            System.out.println("IllegalArgument");
+            System.exit(-1);
+        }
+        while ((iterations * iterations) <= number)
+        {
+            System.out.println(number % iterations);
+            if (number % iterations == 0)
+            {
+                System.out.print("false ");
+                System.out.println(iterations - 1);
+                return;
+            }
+            iterations++;
+        }
+        System.out.print("true ");
+        System.out.println(iterations - 1);
+    }
+}
