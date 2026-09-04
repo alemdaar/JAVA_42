e1a1acf33e3e25feaabc5088905ba527d76137d7
Oussama Elhassouni <oelhasso@c1r5p7.1337.ma>
2026-09-04 11:35:20 +0100

    i misunderstood the subject requirements, i did used kloop and condition operators and subject only Standard operations of primitive types for operators, now i made the subject working only for number 479598

--- JAVA_MODULE_00/ex00/Program.java
+++ JAVA_MODULE_00/ex00/Program.java
@@ -1,18 +1,18 @@
 public class Program {
     public static void main (String[] args)
     {
-        // number must be positive guys ! no need to make a stupid negative check for positive numbers
-        int x = 54321;
-        int sum = 0;
-        while (true)
-        {
-            // System.out.printf("+%d = ", x % 10);
-            sum += x % 10;
-            x /= 10;
-            if (x == 0)
-                break;
-            
-        }
+        int x = 479598;
+        int sum = x % 10;
+        x /= 10;
+        sum += x % 10;
+        x /= 10;
+        sum += x % 10;
+        x /= 10;
+        sum += x % 10;
+        x /= 10;
+        sum += x % 10;
+        x /= 10;
+        sum += x % 10;
         System.out.println(sum);
     }
 }
