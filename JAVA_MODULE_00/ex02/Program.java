865c81bec711f9c8ee116449446628a25a321114
Oussama Elhassouni <oelhasso@c1r5p7.1337.ma>
2026-09-04 15:10:13 +0100

    ive made some changes in the codes logic, the code was consiering 1 and 2 as prime numbers, which are not, so i had to add a check specially for em

--- JAVA_MODULE_00/ex02/Program.java
+++ JAVA_MODULE_00/ex02/Program.java
@@ -5,14 +5,14 @@ public class Program {
     {
         Scanner scanner = new Scanner(System.in);
         int count = 0;
-        // since the value is not 42 that means we didnt reach the EOF
+        // since the value is not 42 that means we continue reading input.
         int number = 0;
         while ((number = scanner.nextInt()) != 42)
         {
             // we set the sum to 0 bc its the variable that we will get the values 
             // of each single number in the number and increament each time
             int each = 0;
-            boolean sign = true;
+            boolean isPrime = true;
             // we will loop over the number to keep divide it each time to 10 
             // till we get 0 so we know its the last part
             // and in the same time we take the reminder of number % 10
@@ -22,16 +22,18 @@ public class Program {
                 number /= 10;
             }
             // now we will see if the number is prime
+            if (each <= 1)
+                continue;
             int iterations = 2;
             while ((iterations * iterations) <= each)
             {
                 if (each % iterations == 0) {
-                    sign = false;
+                    isPrime = false;
                     break;
                 }
                 iterations++;
             }
-            if (sign == false)
+            if (isPrime == false)
                 continue;
             count ++;
         }
